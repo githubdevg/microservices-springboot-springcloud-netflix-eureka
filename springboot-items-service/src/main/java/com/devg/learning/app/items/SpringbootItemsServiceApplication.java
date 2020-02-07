2715b86d7e58ca4f9d491529014f19dd5e9e28ff
Diego Vilchis <zeta_x_07@hotmail.com>
2020-02-06 19:09:10 -0600

    Eureka Server: Implementation of service discovery through Eureka

--- springboot-items-service/src/main/java/com/devg/learning/app/items/SpringbootItemsServiceApplication.java
+++ springboot-items-service/src/main/java/com/devg/learning/app/items/SpringbootItemsServiceApplication.java
@@ -2,10 +2,12 @@ package com.devg.learning.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "products-service")
+//@RibbonClient(name = "products-service") Ribbon impementation no needed with Eureka
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootItemsServiceApplication {
--- springboot-products-service/src/main/java/com/devg/learning/app/products/SpringbootProductsServiceApplication.java
+++ springboot-products-service/src/main/java/com/devg/learning/app/products/SpringbootProductsServiceApplication.java
@@ -2,7 +2,9 @@ package com.devg.learning.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootProductsServiceApplication {
 

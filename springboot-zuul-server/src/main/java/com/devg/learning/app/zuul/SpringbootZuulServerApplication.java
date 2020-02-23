5bc2a6e16c7f53d9a1527f2ac1ddec4544ad73ed
DEVG <DEVG>
2020-02-23 14:30:48 -0600

    Springboot zuul server project initial commit|

---
+++ springboot-zuul-server/src/main/java/com/devg/learning/app/zuul/SpringbootZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.devg.learning.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZuulServerApplication.class, args);
+	}
+
+}

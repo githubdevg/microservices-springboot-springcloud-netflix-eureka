45795340cce301aa49ba935b53f72bab18be362e
DEVG <DEVG>
2020-02-23 16:17:36 -0600

    Implementation of Pre and Post Zuul Filters

--- springboot-zuul-server/src/main/java/com/devg/learning/app/zuul/SpringbootZuulServerApplication.java
+++ springboot-zuul-server/src/main/java/com/devg/learning/app/zuul/SpringbootZuulServerApplication.java
@@ -2,7 +2,10 @@ package com.devg.learning.app.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class SpringbootZuulServerApplication {
 

2d1cf054f38711540160f3aa296377fce032cdd7
Diego Vilchis <diego.vilchis@globant.com>
2020-03-02 10:04:25 -0600

    Spring Cloud Config Server Implementation

--- springboot-items-service/src/main/java/com/devg/learning/app/items/controller/ItemController.java
+++ springboot-items-service/src/main/java/com/devg/learning/app/items/controller/ItemController.java
@@ -1,9 +1,15 @@
 package com.devg.learning.app.items.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +29,12 @@ public class ItemController {
 	@Autowired
 	@Qualifier("itemsServiceFeignImpl")
 	private ItemService itemService;
+
+	@Autowired
+	private Environment env;
 	
+	@Value("${config.description}")
+	private String environmentDescription;
 	
 	@GetMapping("/items")
 	public List<Item> listItems() {
@@ -49,5 +60,18 @@ public class ItemController {
 		item.setQuantity(0);
 		
 		return item;
+	}
+	
+	@GetMapping("/config")
+	public ResponseEntity<?> getConfiguration() {
+		Map<String, Object> response = new HashMap<>();
+		response.put("environmentDescription", environmentDescription);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			String devToken = env.getProperty("config.dev.authentication.token", "Default");
+			response.put("devToken", devToken);
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
 }
